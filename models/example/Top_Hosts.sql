079252f752979f211a16686aa19a9f710e408489
SokhnaSy14 <sokhna-aminata-sy.diouf@atos.net>
2023-09-08 13:12:18 +0200

    dbt deps, generate docs exposur

--- models/example/Top_Hosts.sql
+++ models/example/Top_Hosts.sql
@@ -1,8 +1,14 @@
-select
-l.host_id, room_type,host_name, 
-count(l.host_id) as n_listings
+with source_data as(select
+l.host_id,host_name, 
+ count(case when lower(room_type) LIKE '%entire%' then room_type end) as entire_apt,
+  count(case when lower(room_type) LIKE '%hotel%' then room_type end) as hotel_room,
+  count(case when lower(room_type) LIKE '%private%' then room_type end) as shared_room,
+  count(case when lower(room_type) LIKE '%shared%' then room_type end) as private_room,
+  count(l.host_id) as n_listings
 from listings_airbnb_paris l
 join 
 hosts_airbnb_paris h on l.host_id = h.host_id
-group by l.host_id, room_type,host_name
-order by n_listings desc
+group by l.host_id, host_name
+order by n_listings desc)
+
+select * from source_data
