9cc451ef55e618fe0f153062035a6fc466bceb0c
SokhnaSy14 <sokhna-aminata-sy.diouf@atos.net>
2023-09-09 16:27:17 +0200

    dbt models final

--- models/example/Top_Hosts.sql
+++ models/example/Top_Hosts.sql
@@ -7,7 +7,7 @@ l.host_id,host_name,
   count(l.host_id) as n_listings
 from {{source("airbnb_paris","listings_airbnb_paris")}} l
 join 
-hosts_airbnb_paris h on l.host_id = h.host_id
+{{source("airbnb_paris","hosts_airbnb_paris")}} h on l.host_id = h.host_id
 group by l.host_id, host_name
 order by n_listings desc)
 
--- models/example/activity.sql
+++ models/example/activity.sql
@@ -1,12 +1,17 @@
-with source_data as (
-select
-minimum_nights, avg(price), number_of_reviews
-from {{source("airbnb_paris","listings_airbnb_paris")}}
-),
-
+with source_data as(select availability_365, count(availability_365)  
+from {{source("airbnb_paris","listings_airbnb_paris")}}  
+group by availability_365
+order by availability_365),
 
 final as (
-    select * from source_data
+select * from source_data cross join (select  round(avg(price)) as avg_price_by_night 
+from {{source("airbnb_paris","listings_airbnb_paris")}} )tab
+cross join 
+(select round(avg(minimum_nights)) as avg_minimum_nights 
+from {{source("airbnb_paris","listings_airbnb_paris")}} )tab1
+cross join 
+(select round(avg(minimum_nights)*avg(price)) as avg_income 
+from {{source("airbnb_paris","listings_airbnb_paris")}} )tab2
 )
 
 select * from final
